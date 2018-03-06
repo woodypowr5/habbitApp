e606f07e6ba385ffa45d0196b5edca64e64534b2
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-03-06 14:19:56 -0800

    added new links to sidenav and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,10 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'training', loadChildren: './training/training.module#TrainingModule', canLoad: [AuthGuard] }
+  { path: 'training', loadChildren: './training/training.module#TrainingModule', canLoad: [AuthGuard] },
+  { path: 'tracking', loadChildren: './tracking/tracking.module#TrackingModule', canLoad: [AuthGuard] },
+  { path: 'plan', loadChildren: './plan/plan.module#PlanModule', canLoad: [AuthGuard] },
+  { path: 'trends', loadChildren: './trends/trends.module#TrendsModule', canLoad: [AuthGuard] }
 ];
 
 @NgModule({
