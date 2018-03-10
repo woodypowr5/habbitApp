a6d0c25cd0a6b89059681b8677b2e92aeaa546e6
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-03-09 17:59:02 -0800

    editPlanComponent now gets data from Input

--- src/app/plan/edit-plan/edit-plan.component.ts
+++ src/app/plan/edit-plan/edit-plan.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs/observable';
-import { map } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
 
-import { MarkerService } from '../../shared/marker.service';
-import { PlanService } from './../plan.service';
 import { Marker } from '../../shared/marker.model';
 import { UserData } from './../../auth/userData.model';
-import { UIService } from '../../shared/ui.service';
-import * as fromMarker from '../../shared/marker.reducer';
-import * as fromRoot from '../../app.reducer';
 
 @Component({
   selector: 'app-edit-plan',
@@ -18,24 +11,10 @@ import * as fromRoot from '../../app.reducer';
   styleUrls: ['./edit-plan.component.css']
 })
 export class EditPlanComponent implements OnInit {
-  availableMarkers$: Observable<Marker[]>;
-  isLoading$: Observable<boolean>;
+  @Input() availableMarkers: Marker[];
 
-  constructor(
-    private markerService: MarkerService,
-    private planService: PlanService,
-    private uiService: UIService,
-    private store: Store<fromMarker.State>
+  constructor() {}
 
-  ) {}
-  ngOnInit() {
-    this.isLoading$ = this.store.select(fromRoot.getIsLoading);
-    this.availableMarkers$ = this.store.select(fromMarker.getAvailableMarkers);
-    this.fetchMarkers();
-  }
-
-  fetchMarkers() {
-    this.markerService.fetchAvailableMarkers();
-  }
-
+  ngOnInit() {}
+  
 }
--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { getAvailableMarkers } from './../shared/marker.reducer';
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/observable';
+
+import { MarkerService } from '../shared/marker.service';
+import { PlanService } from './plan.service';
+import { UIService } from './../shared/ui.service';
+import { Plan } from './plan.model';
+import { Marker } from '../shared/marker.model';
+import { UserData } from './../auth/userData.model';
+
+import * as fromMarker from '../shared/marker.reducer';
+import * as fromRoot from '../app.reducer';
+import * as fromPlan from './plan.reducer';
+
 
 @Component({
   selector: 'app-plan',
@@ -6,10 +22,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plan.component.css']
 })
 export class PlanComponent implements OnInit {
+  // private myPlan$: Observable<Plan>;
+  private availableMarkers$: Observable<Marker[]>;
+  isLoading$: Observable<boolean>;
 
-  constructor() { }
+  @Output() availableMarkersChanged = new EventEmitter<Plan>();
+
+  constructor(
+    private markerService: MarkerService,
+    private planService: PlanService,
+    private uiService: UIService,
+    // private store: Store<fromMarker.State>
+    private store: Store<fromPlan.State>
+  ) { }
 
   ngOnInit() {
+    this.availableMarkers$ = this.store.select(fromMarker.getAvailableMarkers);
+    this.availableMarkers$.subscribe(
+      newMarkers => this.myPlanChanged(newMarkers)
+    );
+    this.fetchMyPlan();
+    this.fetchAvailableMarkers();
+  }
+
+  fetchMyPlan() {
+    this.markerService.fetchAvailableMarkers();
+  }
+
+  fetchAvailableMarkers() {
+    this.markerService.fetchAvailableMarkers();
+  }
+
+  myPlanChanged(newMarkers) {
+    console.log(newMarkers)
+    this.availableMarkersChanged.emit(newMarkers);
   }
 
 }
